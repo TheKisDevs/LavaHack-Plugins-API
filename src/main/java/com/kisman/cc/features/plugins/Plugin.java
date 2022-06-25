9ab981721e7b5904c53f2625d57987992ad9704a
It's kisman <81470009+kisman2000@users.noreply.github.com>
2022-06-25 22:40:19 +0300

    v1.3.1(Plugin class is abstract now)

--- src/main/java/com/kisman/cc/features/plugins/Plugin.java
+++ src/main/java/com/kisman/cc/features/plugins/Plugin.java
@@ -6,7 +6,8 @@ package com.kisman.cc.features.plugins;
  * @author _kisman_
  * @since 21:13 of 08.06.2022
  */
-public interface Plugin {
-    void load();
-    void unload();
+public abstract class Plugin {
+    public void init() {/*Idk*/}
+    public abstract void load();
+    public abstract void unload();
 }
